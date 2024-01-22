797224c8113d090e6d5f75e16641e985d4542968
Thiago <thipereira.silva@hotmail.com>
2024-01-22 19:50:44 -0300

    commit aula de loops

--- Aulas/Módulo 2/10dataTimeETempo.py
+++ Aulas/Módulo 2/10dataTimeETempo.py
@@ -19,6 +19,19 @@ data_atual = datetime.now()
 prazo = data_exemplo - data_atual
 print(prazo.days)
 
+#strptime (string parse time)para converter uma string em um objeto 'datetime'
+data_string = "2022-01-11 08:30:00"
+formato = "%Y-%m-%d %H:%M:%S"
+data_objeto = datetime.strptime(data_string, formato)
+print(data_objeto)
+#strftime (string format time)para formatar um objeto 'datetime' como uma string
+data_objeto = datetime(2022, 1, 11, 8, 30)
+formato = "%Y-%m-%d %H:%M:%S"
+data_formatada = data_objeto.strftime(formato)
+print(data_formatada)
+
+
+
 #Desafio
 dia_do_aniversario = datetime(2024, 2, 24)
 dias_pro_aniversario = dia_do_aniversario - datetime.now()
--- Exercicos/parOuImpar.py
+++ Exercicos/parOuImpar.py
@@ -2,8 +2,8 @@ numero = int(input('Digite um numero: ' ))
 while numero > 0:
     if numero % 2 == 0:
         print(f'{numero} PAR')
-        numero = numero - 1
+        numero -= 1
     else:
         print(f'{numero} ÍMPAR')
-        numero = numero - 1
+        numero -= 1
 print('Fim do loop')
